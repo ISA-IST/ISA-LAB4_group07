e2ea8c7e7f533bdcca719ec3e00ad2c249045451
Matteo Fava <72598110+S274920@users.noreply.github.com>
2021-04-05 10:29:57 +0200

    correction 32-->32

--- src/dut_if_MBE.sv
+++ src/dut_if_MBE.sv
@@ -1,9 +1,8 @@
 interface dut_if(input clk, rst);
     logic [31:0] A, B;
-    logic [31:0] data;
+    logic [63:0] data;
     logic valid, ready;
-    
+
     modport port_in (input clk, rst, A, B, valid, output ready);
     modport port_out (input clk, rst, output valid, data, ready);
 endinterface
-
